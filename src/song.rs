4de0fa34c293d6c4138366b3a8ea21787cfe94df
Stipendi <stipendi@users.noreply.github.com>
2020-03-06 10:00:54 +0200

    Add function for reading Song from file

--- src/song.rs
+++ src/song.rs
@@ -1,4 +1,6 @@
+use std::fs;
 use std::fs::File;
+use std::path::PathBuf;
 
 pub struct Breakpoint {
     pub character: char,
@@ -32,4 +34,89 @@ impl Song {
             lyrics,
         }
     }
+}
+
+pub fn try_get_song_from_file(audio_data: File, song_path: PathBuf) -> Option<Song> {
+    let song_data = match fs::read_to_string(song_path) {
+        Ok(value) => value,
+        Err(_) => return None,
+    };
+
+    let mut name_buffer = String::new();
+    let mut finding_name = true;
+
+    let mut bpm_buffer = String::new();
+    let mut finding_bpm = false;
+    let mut bpm: Option<u32> = None;
+    let mut wait_until_newline = false;
+
+    let mut finding_lyrics = false;
+    let mut lyrics_buffer = String::new();
+
+    let mut ignore_whitespace = false;
+
+    for char in song_data.chars() {
+        if ignore_whitespace && (char == '\n' || char == ' ' || char == '\t') {
+            continue;
+        }
+
+        if wait_until_newline {
+            if char == '\n' {
+                wait_until_newline = false;
+                ignore_whitespace = true;
+            }
+            continue;
+        }
+
+        if finding_name && char == '\n' {
+            if name_buffer.is_empty() {
+                return None;
+            }
+            finding_name = false;
+            finding_bpm = true;
+            ignore_whitespace = true;
+            continue;
+        }
+
+        if finding_name {
+            name_buffer.push(char);
+        }
+
+        if finding_bpm {
+            ignore_whitespace = false;
+            if char.is_numeric() {
+                bpm_buffer.push(char);
+            } else {
+                if bpm_buffer.is_empty() {
+                    return None;
+                }
+                finding_bpm = false;
+                match bpm_buffer.parse::<u32>() {
+                    Ok(value) => {
+                        bpm = Some(value);
+                        wait_until_newline = true;
+                        finding_lyrics = true;
+                        continue;
+                    }
+                    Err(_) => return None,
+                }
+            }
+        }
+
+        if finding_lyrics {
+            ignore_whitespace = false;
+            lyrics_buffer.push(char);
+        }
+    }
+
+    if let None = bpm {
+        return None;
+    }
+
+    Some(Song::new(
+        audio_data,
+        name_buffer,
+        bpm.unwrap(),
+        lyrics_buffer,
+    ))
 }
